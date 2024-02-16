362fb20fb2e791eaff17a26e750af3688f9a0d3d
Evan Marmer <evanmarmer@gmail.com>
2024-02-16 10:47:43 -0700

    basic html/css for trips page done

--- src/Trips.jsx
+++ src/Trips.jsx
@@ -1,17 +1,31 @@
+import './Trips.css'
 
 export default function Trips(){
 
 
     return (
         <>
-          <div className='tripName'>Boston</div>
+          <div className="trip-name">Boston</div>
           <div className="header-container1">
-                <div className="task">To-Do</div>
-                <div className="location">Owner</div>
+                <div className="todo">To-Do</div>
+                <div className="owner">Owner</div>
+            </div>
+            <div className='table-container1'>
+                <div className="row-container1">
+                    <button className="">X</button>
+                    <div className=''>To-Do here</div>
+                    <div className="">Owner here</div>
+                </div>
             </div>
             <div className="header-container2">
-                <div className="task"></div>
-                <div className="location">Owner</div>
+                <div className="pin">Pin</div>
+                <div className="place">Place</div>
+            </div>
+            <div className='table-container2'>
+                <div className="row-container2">
+                    <div className=''>Pin here</div>
+                    <div className="">Place here</div>
+                </div>
             </div>
         </>
     )
