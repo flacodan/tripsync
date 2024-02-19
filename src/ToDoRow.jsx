e9458e7547ffdbee25ff8d636ce75b52ebd742a5
Evan Marmer <evanmarmer@gmail.com>
2024-02-19 08:51:34 -0700

    finish css

--- src/ToDoRow.jsx
+++ src/ToDoRow.jsx
@@ -1,22 +1,22 @@
 import './ToDoRow.css'
 import axios from 'axios'
 
-export default function ToDoRow(props){
-//    console.log(props)
-    async function doneTask(e){
-        e.preventDefault()
+export default function ToDoRow(props) {
+    async function doneTask(e) {
+        e.preventDefault();
         let maBod = {
-           todoId: props.todo.to_do_id
-        }
-        // console.log(maBod)
-        
+            todoId: props.todo.to_do_id
+        };
+
         axios.put('/done-task', maBod)
-        .then((response) => {
-            props.setTodoName(response.data)
-            console.log('glarg')
-            console.log(response.data)
-            console.log('blagg')
-        })
+            .then((response) => {
+                props.setTodoName(response.data);
+                console.log('glarg');
+                console.log(response.data);
+                console.log('blagg');
+                // Remove the row from the page
+                e.target.closest('.row-container').remove();
+            });
     }
 
     return (
--- src/Trips.jsx
+++ src/Trips.jsx
@@ -5,27 +5,36 @@ export default function Trips(){
 
     return (
         <>
-          <div className="trip-name">Boston</div>
-          <div className="header-container1">
-                <div className="todo">To-Do</div>
-                <div className="owner">Owner</div>
-            </div>
-            <div className='table-container1'>
-                <div className="row-container1">
-                    <button className="">X</button>
-                    <div className=''>To-Do here</div>
-                    <div className="">Owner here</div>
+            <div className="trip-name">Boston</div>
+            <div className="trip-lists">
+                <div className="trip-list">
+                    <div className="header-container1">
+                        <div className="todo2">To-Do</div>
+                        <div className="owner">Owner</div>
+                    </div>
+                    <div className='table-container1'>
+                        <div className="row-container1">
+                            <button className="YN">X</button>
+                            <div className=''>To-Do here</div>
+                            <div className="">Owner here</div>
+                        </div>
+                    </div>
+                        <button className="to-do-btn">Add To-Do</button>
                 </div>
-            </div>
-            <div className="header-container2">
-                <div className="pin">Pin</div>
-                <div className="place">Place</div>
-            </div>
-            <div className='table-container2'>
-                <div className="row-container2">
-                    <div className=''>Pin here</div>
-                    <div className="">Place here</div>
+                <div className="trip-list">
+                    <div className="header-container2">
+                        <div className="pin">Pin</div>
+                        <div className="place">Place</div>
+                    </div>
+                    <div className='table-container2'>
+                        <div className="row-container2">
+                            <div className=''>Pin here</div>
+                            <div className="">Place here</div>
+                        </div>
+                    </div>
+                        <button className="place-btn">Add Place</button>
                 </div>
+                <button className="notes-btn">Notes</button>
             </div>
         </>
     )
