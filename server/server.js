858e8e33587eef276aa8a97c6b79ef75f9b29b7d
Daniel Griffin <dgriffinpv@gmail.com>
2024-02-23 14:05:00 -0700

    signIn working and added trip code functionality

--- server/server.js
+++ server/server.js
@@ -2,15 +2,29 @@ import express from "express";
 import ViteExpress from "vite-express";
 import { Sequelize, Op } from "sequelize";
 import { User, To_do, Pin, Note, Trip } from "../models/model.js";
+import session from "express-session";
 
 const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({ secret: "work plzz", saveUninitialized: true, resave: false }),
+);
+
 ViteExpress.config({ printViteDevServerHost: true });
 
-app.get("/api/getUsersOpenTrips", async (req, res) => {
+function loginRequired(req, res, next) {
+  console.log(req.session)
+  if (!req.session.user_id) {
+    res.status(401).json({ error: "Unauthorized" });
+  } else {
+    next();
+  }
+}
+
+app.get("/api/getUsersOpenTrips", loginRequired, async (req, res) => {
   // const { user_id } = req.session.user;
   const user_id = 1; // !!!!!!!!!!!!!!!!!!! Must CHANGE this after login is implemented !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   const user = await User.findByPk(user_id);
@@ -26,7 +40,31 @@ app.get("/api/getUsersOpenTrips", async (req, res) => {
   }
 });
 
-app.post("/api/getTrip", async (req, res) => {
+
+app.post("/api/addUserToTrip/:trip_code", async (req, res) => {
+  const trip_code = req.params.trip_code;
+  const user_id = req.session.user_id;
+  console.log("user_id", user_id, "trip_code", trip_code);
+  
+  try {
+    const trip = await Trip.findOne({
+      where: { trip_code: trip_code },
+    });
+
+    if (trip) {
+      await trip.addUser(user_id);
+      res.status(200).send(trip);
+    } else {
+      res.status(404).send({ error: "Trip not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+app.post("/api/getTrip", loginRequired, async (req, res) => {
   // const { user_id } = req.session.user;
   const { trip_id } = req.body;
   const user_id = 1; // !!!!!!!!!!!!!!!!!!! Must CHANGE this after login is implemented !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -43,7 +81,7 @@ app.post("/api/getTrip", async (req, res) => {
   }
 });
 
-app.get("/api/trips/:tripId/todos", async (req, res) => {
+app.get("/api/trips/:tripId/todos", loginRequired, async (req, res) => {
   const tripId = req.params.tripId;
   const todos = await getTodosForTrip(tripId);
   res.json(todos);
@@ -69,7 +107,7 @@ async function getTodosForTrip(tripId) {
   }
 }
 
-app.put("/api/todoUpdate/:id", async (req, res) => {
+app.put("/api/todoUpdate/:id", loginRequired, async (req, res) => {
   // const { username, user_id } = req.session.user;
   const { id } = req.params;
   const { to_do_name, to_do_complete } = req.body;
@@ -83,7 +121,7 @@ app.put("/api/todoUpdate/:id", async (req, res) => {
   res.sendStatus(200);
 });
 
-app.get("/open-to-do", async (req, res) => {
+app.get("/open-to-do", loginRequired, async (req, res) => {
   const todo = await To_do.findAll({
     where: { to_do_complete: false },
     include: [
@@ -97,7 +135,7 @@ app.get("/open-to-do", async (req, res) => {
   res.send(todo);
 });
 
-app.put("/done-task", async (req, res) => {
+app.put("/done-task", loginRequired, async (req, res) => {
   console.log(req.body);
   await To_do.update(
     { to_do_complete: true },
@@ -118,7 +156,7 @@ app.put("/done-task", async (req, res) => {
   res.send(todo);
 });
 
-app.get("/api/getPastTrips", async (req, res) => {
+app.get("/api/getPastTrips", loginRequired, async (req, res) => {
   try {
     const response = await Trip.findAll({
       where: { trip_complete: true },
@@ -129,7 +167,7 @@ app.get("/api/getPastTrips", async (req, res) => {
   }
 });
 
-app.delete("/api/deleteTrip/:id", async (req, res) => {
+app.delete("/api/deleteTrip/:id", loginRequired, async (req, res) => {
   const { id } = req.params;
   try {
     const trip = await Trip.findByPk(id);
@@ -140,7 +178,7 @@ app.delete("/api/deleteTrip/:id", async (req, res) => {
   }
 });
 
-app.post('/api/trips', async (req, res) => {
+app.post('/api/trips', loginRequired, async (req, res) => {
   
   let newTrip = await Trip.create(req.body)
   res.status(200).send(newTrip)
@@ -158,7 +196,10 @@ app.post('/api/signIn', async (req, res) => {
   let user = await User.findOne({where: {username: username, password: password}})
   if (user) {
     // Successful login
-    res.status(200).send(user)
+    req.session.username = user.username;
+    req.session.user_id = user.user_id;
+    req.session.save();
+    res.status(200).send({user: user, success: true})
     console.log('Logged in successfully')
   } else {
       // Failed login
@@ -166,6 +207,11 @@ app.post('/api/signIn', async (req, res) => {
       console.log('Invalid email or password');
   }
 })
+app.post("/logout", loginRequired, async (req, res) => {
+  req.session.destroy();
+  res.send({ success: true });
+});
+
 // didnt need (evan)
 // app.get('/to-do-trip-name', async (req, res) => {
 //   const tripName = await Trip.findAll({
@@ -174,7 +220,7 @@ app.post('/api/signIn', async (req, res) => {
 //   res.send(tripName);
 // })
 
-app.get("/pin-place", async (req, res) => {
+app.get("/pin-place", loginRequired, async (req, res) => {
   const { trip_id } = req.query 
   // console.log('this is my log' + JSON.stringify(trip_id));
 
@@ -200,7 +246,7 @@ app.post("/coord", async (req, res) => {
 res.send(donePin)
 })
 
-app.get("/pin-place", async (req, res) => {
+app.get("/pin-place", loginRequired, async (req, res) => {
   const pins = await Pin.findAll({
     where: { trip_id: 1 },
   });
--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,12 +1,15 @@
 
 import { React, useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import  {Link}  from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
 
 const SignIn = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [signedIn, setSignedIn] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,7 +20,10 @@ const SignIn = () => {
         axios.post('/api/signIn', userBody)
         .then((response) => {
             console.log(response.data);
-            setSignedIn(true);
+            if (response.data) {
+                setSignedIn(true);
+                navigate('/home', { userObj: response.data });
+            }
         })
         console.log('submitted');
     };
@@ -63,56 +69,4 @@ const SignIn = () => {
     )
     
 };
-
-
-//   return (
-    //     <div>
-    //       <h1>Login</h1>
-    //       <form onSubmit={handleSubmit}>
-    //         <label htmlFor="email">Email:</label>
-    //         <input
-    //           type="email"
-    //           id="email"
-    //           value={email}
-    //           onChange={handleEmailChange}
-    //         />
-    
-    //         <label htmlFor="password">Password:</label>
-    //         <input
-    //           type="password"
-    //           id="password"
-    //           value={password}
-    //           onChange={handlePasswordChange}
-    //         />
-    
-    //         <button type="submit">Sign In</button>
-    //       </form>
-    //     </div>
-    //   );
-    // };
-    
-    // return (
-    //     <div>
-    //         <h1>Login</h1>
-    //         <form onSubmit={handleSubmit}>
-    //             <label htmlFor="email">Email:</label>
-    //             <input
-    //                 type="email"
-    //                 id="email"
-    //                 value={email}
-    //                 onChange={handleEmailChange}
-    //             />
-    
-    //             <label htmlFor="password">Password:</label>
-    //             <input
-    //                 type="password"
-    //                 id="password"
-    //                 value={password}
-    //                 onChange={handlePasswordChange}
-    //             />
-    
-    //             <button type="submit">Sign In</button>
-    //         </form>
-    //     </div>
-    // );
     export default SignIn;
